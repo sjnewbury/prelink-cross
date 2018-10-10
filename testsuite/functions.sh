bd6e69d4f446253285118b4ef0740918a34a59c4
Sergei Trofimovich <slyfox@gentoo.org>
2018-10-10 21:50:10 +0100

    testsuite/functions.sh: produce deterministic GNU_PRELINK section
    Before the change one test failed due to timestamp drift:
      FAIL: shuffle7.sh
    
    This happens because prelinking takes time on the file
    and timestamp differs slightly.
    
    Avoid timestamp drift by overriding real timestamp
    with PRELINK_TIMESTAMP variable.
    
    Signed-off-by: Sergei Trofimovich <slyfox@gentoo.org>
    
    Added ChangeLog entry
    
    Signed-off-by: Mark Hatle <mark.hatle@windriver.com>

--- testsuite/functions.sh
+++ testsuite/functions.sh
@@ -4,6 +4,12 @@ CCLINK=${CCLINK:-${CC} -Wl,--dynamic-linker=`echo ./ld*.so.*[0-9]`}
 CXX="${CXX:-g++} ${LINKOPTS}"
 CXXLINK=${CXXLINK:-${CXX} -Wl,--dynamic-linker=`echo ./ld*.so.*[0-9]`}
 PRELINK=${PRELINK:-../src/prelink -c ./prelink.conf -C ./prelink.cache --ld-library-path=. --dynamic-linker=`echo ./ld*.so.*[0-9]` --rtld=../src/rtld/prelink-rtld}
+
+# force deterministic timestamps to make double prelinking
+# to produce unmodified binary.
+PRELINK_TIMESTAMP=${PRELINK_TIMESTAMP:-12345678}
+export PRELINK_TIMESTAMP
+
 LDD=${LDD:-../src/rtld/prelink-rtld}
 STRIP=${STRIP:-strip}
 HOST_CC=${HOST_CC:-$CC}
